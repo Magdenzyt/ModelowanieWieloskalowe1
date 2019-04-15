db38f3a8c4cfa03be1e9031ebd809c6b0473c549
MagdaWu <wumagda16@gmail.com>
2019-04-15 19:24:59 +0200

    Initial commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(15, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(624, 347);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(671, 173);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(590, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Wymiar";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(671, 240);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(125, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(590, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Reguła";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 528);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap DrawArea;
+        Graphics g;
+        SolidBrush pinkBrush = new SolidBrush(Color.HotPink);
+
+        int elements;
+        int size;
+        int wersja;
+        int epoki = 100;
+
+        List<Boolean> beginTab;
+        List<bool> nextTab;
+
+        int lNeighbour;
+        int rNeighbour;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            DrawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            pictureBox1.Image = DrawArea;
+            g = Graphics.FromImage(DrawArea);
+
+            comboBox1.Items.Add(30);
+            comboBox1.Items.Add(60);
+            comboBox1.Items.Add(90);
+            comboBox1.Items.Add(120);
+            comboBox1.Items.Add(255);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            g.Clear(Color.Transparent);
+            beginTab = new List<bool>();
+            nextTab = new List<bool>();
+            if (!(string.IsNullOrWhiteSpace(textBox1.Text) && string.IsNullOrWhiteSpace(comboBox1.Text)))
+            {
+                elements = int.Parse(textBox1.Text);
+                wersja = int.Parse(comboBox1.Text);
+
+                string wersjaBin = "00000000" + Convert.ToString(wersja, 2);
+                size = pictureBox1.Size.Width / elements;
+
+                beginTab = Enumerable.Repeat(false, elements).ToList();
+                nextTab = Enumerable.Repeat(false, elements).ToList();
+
+                beginTab[(int)elements / 2] = true;
+
+                for (int k = 0; k < elements; k++)
+                {
+                    if (beginTab[k] == true)
+                        g.FillRectangle(pinkBrush, size * k, 0, size, size);
+                }
+
+                for (int j = 0; j < epoki; j++)
+                {
+                    for (int i = 0; i < elements; i++)
+                    {
+                        if (i == 0)
+                        {
+                            lNeighbour = elements - 1;
+                            rNeighbour = 1;
+                        }
+                        else if (i == elements - 1)
+                        {
+                            lNeighbour = elements - 2;
+                            rNeighbour = 0;
+                        }
+                        else
+                        {
+                            lNeighbour = i - 1;
+                            rNeighbour = i + 1;
+                        }
+
+                        string trzyBit = "";
+                        trzyBit += beginTab[lNeighbour] ? '1' : '0';
+                        trzyBit += beginTab[i] ? '1' : '0';
+                        trzyBit += beginTab[rNeighbour] ? '1' : '0';
+
+                        int pozycja = Convert.ToInt32(trzyBit, 2);
+
+                        if (wersjaBin[wersjaBin.Length - pozycja - 1] == '1')
+                        {
+                            g.FillRectangle(pinkBrush, size * i, size * (j + 1), size, size);
+                            nextTab[i] = true;
+                        }
+
+                    }
+                    for (int r = 0; r < elements; r++)
+                    {
+                        beginTab[r] = nextTab[r];
+                        nextTab[r] = false;
+                    }
+                    pictureBox1.Image = DrawArea;
+                }
+
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
